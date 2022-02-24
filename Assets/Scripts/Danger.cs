ce66145ab2f883891042e6d02a0b5ea4f632a0b0
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2022-02-24 17:45:01 -0300

    Ui Updates

---
+++ Assets/Scripts/Danger.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Danger : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update() { }
+}
